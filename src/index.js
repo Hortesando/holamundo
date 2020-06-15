6c4ac9b8f4c7b6ac7ebb5917d34d1e5ddd1a4b6d
Hortensia Sandoval Gonzalez <hortensia.sandoval@softtek.com>
2020-06-14 19:34:08 -0500

    Practicando ejercicios

--- src/index.js
+++ src/index.js
@@ -1,18 +1,45 @@
-//importado de librerias, modulos y componentes
-import React from 'react' 
-import ReactDOM from 'react-dom'
-import App from './componentes/App'
+// Importado de librerias, modulos y componentes
+import React from 'react'
+import ReactDom from 'react-dom'
 
 
+/* crear componente, notas para la pagina
 
+const App = () => {
+    return <div></div>
+}
+function App() {
+    return <div></div>
+}
 
+//renderizado de componente
 
-// renderizado de componente
-    ReactDOM.render (
-        <App/>,
-document.querySelector('#root')
+ReactDom.render(
+    <App/>,
+    document.querySelector('#root')
+)*/
 
-    )
+class App extends React.Component{
+    constructor() {
+        super()
+    }
+        render () {
+            return ( 
+            <div>
 
+                <h1>
+                    Hola
+                </h1>
 
+                <h2>
+                    Mundo
+                </h2> 
+            </div>
+            )
+        }
     
+}
+ReactDom.render(
+    <App/>,
+    document.querySelector('#root')
+)
