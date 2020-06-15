f8f4cf3c888161db28a717d5bf21f44ed15875c2
Hortensia Sandoval Gonzalez <hortensia.sandoval@softtek.com>
2020-06-14 20:35:56 -0500

    Cambio de nombres por boton

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -7,7 +7,22 @@ class App extends React.Component{
             name: 'Sebastian',
             lastName: 'Ruiz'
         }
+        this.changeHortensia = this.changeHortensia.bind (this)
+        this.changeAngeles = this.changeAngeles.bind (this)
+        this.changeEstela = this.changeEstela.bind (this)
     }
+    changeHortensia(){
+        this.setState ({name: 'Hortensia', lastName:'Sandoval' })
+        
+    }
+    changeAngeles(){
+        this.setState ({name: 'Angeles', lastName:'Sandoval' })
+    }
+
+    changeEstela(){
+        this.setState ({name: 'Estela', lastName:'Martinez' })
+    }   
+        
         render () {
             return ( 
             <div>
@@ -15,9 +30,11 @@ class App extends React.Component{
                 <h1>
                     Hola { this.state.name + ' ' + this.state.lastName }
                 </h1>
-                <button>Hortensia</button>
-                <button>Estela</button>
-                <button>Angeles</button>
+                <button onClick = {this.changeHortensia}>Hortensia</button>
+                <button onClick = {this.changeAngeles}>Angeles</button>
+                <button onClick = {this.changeEstela}>Estela</button>
+               
+                
             </div>
             )
         }
