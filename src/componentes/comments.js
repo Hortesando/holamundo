e044c98c52bcda875562fc14d31fc5f73f76aa88
Hortensia Sandoval Gonzalez <hortensia.sandoval@softtek.com>
2020-06-15 01:57:33 -0500

    Practica comments

--- src/componentes/comments.js
+++ src/componentes/comments.js
@@ -10,16 +10,14 @@ class Comments extends React.Component{
                         <div className="ui comments">
             <div classNameName="comment">
                 <a className="avatar">
-                    <img href= {faker.image.avatar} alt = "Imagen de usuario"/>
+                    <img src= {faker.image.avatar()} alt = "Imagen de usuario"/>
                 </a>
                 <div className="content">
-                <a className="author">Stevie Feliciano</a>
+                <a className="author"> {faker.name.firstName ()} </a>
                 <div className="metadata">
                         <div className="date">2 days ago</div>
-                        <div className="rating">
-                        <i className="star icon"></i>
-                        5 Faves
-                        </div>
+                        
+                        
                 </div>
                 <div className="text">
                     Hey guys, I hope this example comment is helping you read this documentation.
